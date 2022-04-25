7dbb4781f131f9bdf8b53d789033afb675dd37f5
felipemr <felipemr94@outlook.com>
2022-04-25 13:24:29 -0300

    Initial Commit

---
+++ JSSwift/JSSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSSwiftApp.swift
+//  JSSwift
+//
+//  Created by Felipe Marques on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct JSSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
